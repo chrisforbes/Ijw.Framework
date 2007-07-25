83a3c361ac150bbd854441c2f5caab91db8e0797
(no author) <(no author)@993157c7-ee19-0410-b2c4-bb4e9862e678>
2007-07-25 07:33:52 +0000

    LOTS of hax, IjwPofiler 0.4
    git-svn-id: svn://svn.ijw.co.nz/svn/IjwFramework@1348 993157c7-ee19-0410-b2c4-bb4e9862e678

--- IjwFramework/Delegates/Delegates.cs
+++ IjwFramework/Delegates/Delegates.cs
@@ -1,7 +1,52 @@
+using System;
+using System.Collections.Generic;
+
 namespace IjwFramework.Delegates
 {
 	public delegate T Provider<T>();
 	public delegate T Provider<T, U>(U u);
 	public delegate T Provider<T, U, V>(U u, V v);
 	public delegate void Action();
+
+	// predicates, and simple combinators over predicates
+
+	public static class Predicates
+	{
+		public static bool Always<T>( T ignored ) { return true; }
+		public static bool Never<T>(T ignored) { return false; }
+
+		public static Predicate<T> Or<T>(params Predicate<T>[] a)
+		{
+			return delegate(T t)
+			{
+				foreach (Predicate<T> p in a)
+					if (p(t))
+						return true;
+				return false;
+			};
+		}
+
+		public static Predicate<T> And<T>(params Predicate<T>[] a)
+		{
+			return delegate(T t)
+			{
+				foreach (Predicate<T> p in a)
+					if (!p(t))
+						return false;
+				return false;
+			};
+		}
+
+		public static Predicate<T> Not<T>(Predicate<T> p)
+		{
+			return delegate(T t) { return !p(t); };
+		}
+	}
+
+	// helper iterators
+
+	public static class Iterators
+	{
+		public static IEnumerable<T> Yield<T>(params T[] t) { return t; }
+	}
 }
