f59561081b96c34bebb60781987c36156b9b9838
chrisf <chrisf@993157c7-ee19-0410-b2c4-bb4e9862e678>
2008-06-20 04:43:55 +0000

    git-svn-id: svn://svn.ijw.co.nz/svn/IjwFramework@1995 993157c7-ee19-0410-b2c4-bb4e9862e678

---
+++ Ijw.Framework.v35/Class1.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Ijw.Framework.v35
+{
+	public static class Extensions
+	{
+		/// <summary>
+		/// Shorthand for string.Format()
+		/// </summary>
+		public static string F(this string s, params object[] args)
+		{ return string.Format(s, args); }
+
+		/// <summary>
+		/// Repeats the specified sequence. For example, [1,2,3] becomes [1,2,3, 1,2,3, 1,...]
+		/// </summary>
+		public static IEnumerable<T> Cycle<T>(this IEnumerable<T> seq)
+		{
+			for (; ; )
+				foreach (var t in seq) yield return t;
+		}
+	}
+}
