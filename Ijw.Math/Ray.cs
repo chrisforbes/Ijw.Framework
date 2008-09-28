74cb22ef2e91e3166add7bb259db2c6ae2b99e3c
chrisf <chrisf@993157c7-ee19-0410-b2c4-bb4e9862e678>
2008-09-28 19:30:15 +0000

    git-svn-id: svn://svn.ijw.co.nz/svn/IjwFramework@2145 993157c7-ee19-0410-b2c4-bb4e9862e678

---
+++ Ijw.Math/Ray.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Ijw.Math
+{
+	public struct Ray
+	{
+		public Vector3 Origin;
+		public Vector3 Direction;
+
+		public Ray(Vector3 origin, Vector3 direction)
+		{
+			Origin = origin;
+			Direction = direction;
+		}
+
+		public static Ray operator *(Ray r, Matrix m)
+		{
+			return new Ray(
+				r.Origin.TransformAsCoordinate(m),
+				r.Direction.TransformAsNormal(m));
+		}
+	}
+}
