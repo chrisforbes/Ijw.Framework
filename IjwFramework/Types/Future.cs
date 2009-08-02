121bbdfd0ef9cb8b728aa75037c211a28b4af72c
chrisf <chrisf@993157c7-ee19-0410-b2c4-bb4e9862e678>
2009-08-02 20:04:38 +0000

    helps to add the file; futures support
    git-svn-id: svn://svn.ijw.co.nz/svn/IjwFramework@2409 993157c7-ee19-0410-b2c4-bb4e9862e678

---
+++ IjwFramework/Types/Future.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace IjwFramework.Types
+{
+    public class Future<T>
+    {
+        Func<T> f;
+        IAsyncResult async;
+        T value;
+        bool hasValue = false;
+
+        public Future(Func<T> f) { this.f = f; async = f.BeginInvoke(null, null); }
+
+        T Value
+        {
+            get
+            {
+                if (!hasValue)
+                {
+                    if (!async.IsCompleted) async.AsyncWaitHandle.WaitOne();
+                    value = f.EndInvoke(async);
+                    hasValue = true;
+                }
+                return value;
+            }
+        }
+
+        public bool HasValue { get { return hasValue || async.IsCompleted; } }
+        public static implicit operator T(Future<T> f) { return f.Value; }
+    }
+
+    public static class Future
+    {
+        public static Future<T> New<T>(Func<T> f) { return new Future<T>(f); }
+    }
+}
